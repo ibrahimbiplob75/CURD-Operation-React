a2d28d9cad32ed28343c5e0e5b6230fcdad43873
ibrahim75 <ibrahimbiplob75@gmail.com>
2024-06-15 22:20:05 +0600

    Create option done From CRUD

--- express-CURD -server/index.js
+++ express-CURD -server/index.js
@@ -27,6 +27,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const users = client.db("userDB").collection("users");
+
+    app.post("/users",async(req,res)=>{
+      const user=req.body;
+      console.log(user)
+      const result = await users.insertOne(user);
+      res.send(result)
+    })
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
