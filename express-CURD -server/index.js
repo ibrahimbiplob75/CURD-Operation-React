89ea5d70eb81fced24c4a073ef6e9cf582dabb54
ibrahim75 <ibrahimbiplob75@gmail.com>
2024-06-16 10:57:36 +0600

    CURD Operation Delete Implement

--- express-CURD -server/index.js
+++ express-CURD -server/index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const cors=require("cors")
 const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000;
 
@@ -35,6 +35,22 @@ async function run() {
       const result = await users.insertOne(user);
       res.send(result)
     })
+
+    app.get("/users",async(req,res)=>{
+      const result=await users.find().toArray()
+      res.send(result)
+    })
+
+    app.delete("/users/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={
+        _id:new ObjectId(id)
+      }
+      const result=await users.deleteOne(query)
+      res.send(result)
+    })
+      
+    
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
